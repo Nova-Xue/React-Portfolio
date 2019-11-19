e29e15c01c7429db0296028ba0b8c6ade2c9a3a7
Nova <xueshengyuan1118@hotmail.com>
2019-11-19 16:41:39 -0500

    Add links and images

--- portfolio/src/views/Projects/index.js
+++ portfolio/src/views/Projects/index.js
@@ -5,30 +5,31 @@ import View from "../../components/Grid";
 class Projects extends Component {
     state ={
         projects : [{
-            name : "1",
+            name : "5chan",
             link : "1",
+            git : "https://github.com/Nova-Xue/5chan",
             preview : "1"
         },{
-            name : "2",
+            name : "InstaPic",
             link : "2",
-            preview : "2"
+            git : "https://github.com/Nova-Xue/InstaPic",
+            preview : "/img/InstaPic Preview.png"
         },{
-            name : "1",
-            link : "1",
-            preview : "1"
+            name : "Bamazon",
+            git : "https://github.com/Nova-Xue/Bamazon",
+            link : "https://drive.google.com/file/d/11mUSBvL6qgXq7dm2SDcfahVat6B8hNTZ/view",
+            preview : "/img/Bamazon Preview.png"
         },{
-            name : "1",
-            link : "1",
-            preview : "1"
+            name : "Lori-Node",
+            git : "https://github.com/Nova-Xue/Lori-Node-App",
+            link : "https://drive.google.com/file/d/1vxXlhZV3CoezzIImQ22miiKr2JsQHpZO/view",
+            preview : "/img/Lori Preview.png"
         },{
-            name : "1",
-            link : "1",
-            preview : "1"
-        },{
-            name : "1",
-            link : "1",
-            preview : "1"
-        }]
+            name : "I Love Ice Cream",
+            git : "https://github.com/Nova-Xue/I-Love-Icecream",
+            link : "https://nova-xue.github.io/I-Love-Icecream/",
+            preview : "/img/Ice Preview.png"
+        },]
     }
     render() {
         return(
@@ -36,9 +37,14 @@ class Projects extends Component {
             <Row>
             {
                 this.state.projects.map(project => (
-                    <Col md={4}>
-                        name: {project.name}
-                        link : {project.link}
+                    <Col md={4} className="flex-colum border p-2">
+                        <h3>{project.name}</h3>
+                        <div className="link">
+
+                        <a href={project.git}>{project.git}</a>
+                        </div>
+                        <div><a href={project.link} target="_blank"><img src={project.preview} alt="preview"></img></a></div>
+                        
                     </Col>
                     )
                 )
